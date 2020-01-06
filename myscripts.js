e6d1e7423ea6db04ba4d0b96cbfb596b3aff5b1a
Camryn Buonamassa <camrynbuonamassa@gmail.com>
2020-01-06 14:57:26 -0500

    finished questions 3-6

--- myscripts.js
+++ myscripts.js
@@ -13,3 +13,22 @@ var myAlert = function(){
 	alert("Hey, I told you not to hover over me!");
 }
 document.getElementById("hover").addEventListener("mouseover", myAlert);
+
+//Question 4
+var checkpw = function(){
+	var pw = document.getElementById("pw");
+	if(pw.value == 12345678){
+		document.getElementsByTagName("h1")[0].innerHTML = "changed h1";
+	}
+	else{
+		alert("Incorrect password");
+	}
+}
+document.getElementById("submitbutton").addEventListener("click", checkpw);	
+
+//Question 6
+var volume = function(){
+	var radius = document.getElementById("radius");
+	alert(Math.pow(radius.value, 3) * 4 * Math.PI / 3);
+}
+document.getElementById("submitbutton1").addEventListener("click", volume);	
